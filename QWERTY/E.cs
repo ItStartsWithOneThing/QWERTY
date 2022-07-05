a719fc9db30f0903ce3365c22bf21bead93f4dc7
Konstantin Turkin <like69.kt@gmail.com>
2022-07-05 23:44:58 +0300

    Implemented E

---
+++ QWERTY/E.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QWERTY
+{
+    public class E
+    {
+        public int Id { get; set; }
+        public string Position { get; set; }
+    }
+}
